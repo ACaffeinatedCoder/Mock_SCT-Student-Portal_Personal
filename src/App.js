a3e51b39e8b616c435ed996b990bb9a06c8c2e08
ACaffeinatedCoder <97655140+ACaffeinatedCoder@users.noreply.github.com>
2023-03-19 10:53:39 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import {Route, Routes} from "react-router-dom"
 import Login from "./pages/Login"
 import Main from "./pages/Main";
 
-//npm i router-dom
+//Download dependencies first
+// npm install
+// npm i router-dom
 
 function App() {
   // set site title
